45fa4b026864a9e0709a4623efe82dcb1967aa36
maiki0419 <95464965+maiki0419@users.noreply.github.com>
2022-02-19 02:13:35 +0000

    追加

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -5,27 +5,27 @@ class Public::OrdersController < ApplicationController
   end
 
   def confirm
-    # @order = Order.new(order_params)
-    # @order.customer_id = current_customer.id
+    @order = Order.new(order_params)
+    @order.customer_id = current_customer.id
 
-    # if params[:address_option] == 0
-    #   @order.name = current_customer.name
-    #   @order.post_cord = current_customer.post_cord
-    #   @order.address = current_customer.address
-    # elsif params[:address_option] == 1
-    #   @address = Address.find(params[:address_book])
-    #   @order.name = @address.name
-    #   @order.post_cord = @address.post_cord
-    #   @order.address = @address.address
-    # elsif params[:address_option] == 2
-    #   @order.name = params[:name]
-    #   @order.post_cord = params[:post_cord]
-    #   @order.address = params[:address]
-    # end
+    if params[:address_option] == 0
+      @order.name = current_customer.name
+      @order.post_cord = current_customer.post_cord
+      @order.address = current_customer.address
+    elsif params[:address_option] == 1
+      @address = Address.find(params[:address_book])
+      @order.name = @address.name
+      @order.post_cord = @address.post_cord
+      @order.address = @address.address
+    elsif params[:address_option] == 2
+      @order.name = params[:name]
+      @order.post_cord = params[:post_cord]
+      @order.address = params[:address]
+    end
 
-    # @order.total_payment = params[:total_price].to_i + @order.postage
+    @order.total_payment = params[:total_price].to_i + @order.postage
 
-    # @cart_items = current_customer.cart_items
+    @cart_items = current_customer.cart_items
 
 
   end
