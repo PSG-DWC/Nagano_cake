2e86228b6fe84e727b927681176358b11eba7469
Hinano-Tanaka <95991554+Hinano-Tanaka@users.noreply.github.com>
2022-02-20 06:59:07 +0000

    管理者注文履歴一覧

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,5 +1,11 @@
 class Admin::OrdersController < ApplicationController
+ 
+ def index
+  @orders = Order.all.order(created_at: :desc).page(params[:page]).per(10)
+ end
+ 
  def show
+  
  end
 
  def update
