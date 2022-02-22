9c24e335a72270e1f96def5e27e770ff9fd788d2
maiki0419 <95464965+maiki0419@users.noreply.github.com>
2022-02-22 01:20:10 +0000

    管理者フラッシュメッセージ追加

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -2,12 +2,14 @@
 
 class Admin::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
-  
+
   def after_sign_out_path_for(resource)
+    flash[:notice] = "ログアウトに成功しました。"
     new_admin_session_path
   end
-  
+
   def after_sign_in_path_for(resource)
+    flash[:notice] = "ログインに成功しました。"
     admin_orders_path
   end
 
