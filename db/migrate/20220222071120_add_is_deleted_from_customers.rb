6a869490345396187db889323ee2f223491eefce
EC2 Default User <ec2-user@ip-172-31-4-222.ec2.internal>
2022-02-22 07:49:02 +0000

    変更

---
+++ db/migrate/20220222071120_add_is_deleted_from_customers.rb
@@ -0,0 +1,6 @@
+class AddIsDeletedFromCustomers < ActiveRecord::Migration[6.1]
+  def change
+    add_column :customers, :is_deleted, :boolean,  default: false, null: false
+  end
+end
+
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_18_120520) do
+ActiveRecord::Schema.define(version: 2022_02_22_071120) do
 
   create_table "addresses", force: :cascade do |t|
     t.integer "customer_id", null: false
